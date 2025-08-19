e4243557071ae2507b54777a6ea2c262ff282064
cki6090 <52525423+cki6090@users.noreply.github.com>
2025-08-19 14:05:40 +0900

    -
    -

--- app/components/[id]/page.js
+++ app/components/[id]/page.js
@@ -24,7 +24,6 @@ export default function Components() {
       setMenuList(response.data);
       setComponentContents(response2.data);
     };
-
     fetchMenuList();
   }, []);
 
--- app/page.js
+++ app/page.js
@@ -1,3 +1,34 @@
 export default function Home() {
-  return <div>메인</div>;
+  return (
+    <div className="index-page">
+      <div className="index-page-content">
+        <h1>
+          Project Name
+          <br></br>
+          <span>Component Page</span>
+        </h1>
+        <p>
+          This page is a service that helps anyone create a homepage easily and{" "}
+          <br></br>
+          quickly. With an intuitive interface, you can immediately implement{" "}
+          <br></br>
+          the design you want.
+        </p>
+      </div>
+
+      <div className="index-page-image">
+        <ul className="thumbs">
+          <li>
+            <img src="/indeximg1.jpg" className="indeximg1" />
+          </li>
+          <li>
+            <img src="/indeximg2.jpg" className="indeximg2" />
+          </li>
+          <li>
+            <img src="/indeximg3.jpg" className="indeximg3" />
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
